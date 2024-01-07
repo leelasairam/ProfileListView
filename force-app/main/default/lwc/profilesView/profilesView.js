fc54a968931d23d2c72ea0a31287da013359e3cc
leelasairam <saikodavali0948@gmail.com>
2024-01-07 14:19:28 +0530

    added laoder and search

--- force-app/main/default/lwc/profilesView/profilesView.js
+++ force-app/main/default/lwc/profilesView/profilesView.js
@@ -2,6 +2,8 @@ import { LightningElement, track } from 'lwc';
 import GetProfiles from '@salesforce/apex/ProfileList.GetProfiles';
 export default class ProfilesView extends LightningElement {
     @track Profiles;
+    @track FilterProfiles;
+    @track load = false;
     cols = [
         { label: 'Id', fieldName: 'Id' },
         { label: 'Name', fieldName: 'Name' },
@@ -14,6 +16,7 @@ export default class ProfilesView extends LightningElement {
     }
 
     async RetriveProfiles(){
+        this.load = true;
         await GetProfiles()
         .then((result) => {
             const PList = [];
@@ -22,10 +25,29 @@ export default class ProfilesView extends LightningElement {
                 PList.push({profile : i, users : result[i]})
             })
             this.Profiles = PList;
+            this.GetFilterProfiles();
+            this.load = false;
         })
         .catch((error)=>{
             console.log(error);
-        })
+        });
+    }
+
+    GetFilterProfiles(){
+        let key = this.template.querySelector(".filter").value;
+        if(!key){
+            this.FilterProfiles = [...this.Profiles];
+        }
+        else{
+            this.FilterProfiles = this.Profiles.filter((i) => i.profile.toLowerCase().includes(key.toLowerCase()));
+        }
+    }
+
+    Spinner(){
+        this.load = true;
+        setTimeout(() => {
+            this.load = false;
+        },1000)
     }
 
 }
